ad682381107ecffea09c1a6c78400ba55d2184fd
Felipe Olavarría <178.felipe@gmail.com>
2021-06-22 13:41:32 -0400

    Add pdf sign method on on_created callback, delete all event callbacks except on_created in file_observer

--- src/file_observer.py
+++ src/file_observer.py
@@ -1,21 +1,20 @@
 from watchdog.observers import Observer
 from watchdog.events import PatternMatchingEventHandler
-
+from src.signer import sign_pdf
+# from src.qrcode import create_qrcode, insert_qrcode
+SIGNED_PDF_SUFFIX = '-signed.pdf'
 
 def on_created(event):
-    print(f"hey, {event.src_path} has been created!")
+    if event.src_path.endswith(SIGNED_PDF_SUFFIX):
+        return
 
-
-def on_deleted(event):
-    print(f"what the f**k! Someone deleted {event.src_path}!")
-
-
-def on_modified(event):
-    print(f"hey buddy, {event.src_path} has been modified")
-
-
-def on_moved(event):
-    print(f"ok ok ok, someone moved {event.src_path} to {event.dest_path}")
+    # file_name = event.src_path.split('\\')[2]
+    print(f"Archivo {event.src_path} ha sido detectado")
+    try:
+        sign_pdf(event.src_path, event.src_path.replace('.pdf', SIGNED_PDF_SUFFIX))
+    except Exception:
+        print("No se pudo firmar el archivo")
+    print(f"El archivo {event.src_path} ha sido firmado exitosamente")
 
 
 def create_file_observer(folder):
@@ -30,9 +29,6 @@ def create_file_observer(folder):
         case_sensitive
     )
     my_event_handler.on_created = on_created
-    my_event_handler.on_deleted = on_deleted
-    my_event_handler.on_modified = on_modified
-    my_event_handler.on_moved = on_moved
     path = folder
     go_recursively = True
     file_observer = Observer()
